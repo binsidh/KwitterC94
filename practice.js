9f379a3e5aab224f01f2ada0bb83ab4e4a4d5a86
binsidh <58246686+binsidh@users.noreply.github.com>
2021-09-15 19:32:14 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,22 @@
+
+//ADD YOUR FIREBASE LINKS
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAkJoatVCFmCdPhXpS5eUwiM2Ev3ZhR0G0",
+    authDomain: "kwitter-b149a.firebaseapp.com",
+    projectId: "kwitter-b149a",
+    storageBucket: "kwitter-b149a.appspot.com",
+    messagingSenderId: "611980912134",
+    appId: "1:611980912134:web:d446e6eca16142e107c587"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  function addUser()
+  {
+      user_name=document.getElementById("user_name").value;
+      firebase.database().ref("/").child(user_name).update({
+          purpose : "adding user"
+      });
+  }
